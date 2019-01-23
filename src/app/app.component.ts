7d2daebd1a57f3ab23ba73b1f42308d1d95e9ddc
Jake K <jake.kopenhefer@gmail.com>
2019-01-23 14:14:00 -0700

    added page not found page and toggle login/out based on authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthGuardGuard } from './auth-guard.guard';
+import { AuthService } from './login/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  public authenticated;
+
   public appPages = [
     // {
     //   title: 'Home',
@@ -35,9 +39,12 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private auth: AuthService
   ) {
     this.initializeApp();
+    this.auth.isAuthenticated().subscribe( x => this.authenticated = x);
+    console.log(this.authenticated);
   }
 
   initializeApp() {
