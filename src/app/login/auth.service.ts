ec56bca70440224cd9af4f24c952e9944c4fb5fb
Jacob Bennett <kf7ags@gmail.com>
2019-01-24 09:33:55 -0700

    Authentication now creates a doc for user or retrieves one

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { auth } from 'firebase/app';
 import { map } from 'rxjs/operators';
+import {User} from "../shared/user";
+import {FirebaseService} from "../user-list/firebase.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,45 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   auth;
+  public userData: User = {
+    name: '',
+    id: '',
+    movieList: []
+  };
 
   constructor(
-      public afAuth: AngularFireAuth
+      public afAuth: AngularFireAuth,
+      private firebase: FirebaseService
   ) {
 
   }
 
 
   googleSignIn() {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+        .then(authUserData => {
+          this.userData.id = authUserData.user.uid;
+          this.userData.name = authUserData.user.displayName;
+          // console.log(this.userData);
+          this.firebase.checkUser(this.userData.id)
+              .subscribe(theBool => {
+                console.log('the bool is: ');
+                console.log(theBool);
+                if(theBool){
+                  this.firebase.retrieveUser(this.userData.id)
+                      .subscribe(dbUserData =>{
+                        console.log('dbUserData is: ');
+                        console.log(dbUserData);
+                        this.userData.movieList = dbUserData.movieList;
+                        console.log('userData is now: ');
+                        console.log(this.userData);
+                      })
+                }
+                else{
+                  this.firebase.addUser(this.userData);
+                }
+              })
+        });
   }
 
   signOut() {
