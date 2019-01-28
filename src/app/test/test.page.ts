7af44e22650bb1cf3ee866bfd7d7fe76bf9b457c
Jacob Bennett <kf7ags@gmail.com>
2019-01-28 10:41:29 -0700

    updateMLHasSeen on firestore service with new DB protocols

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -3,6 +3,10 @@ import {FirebaseService} from "../user-list/firebase.service";
 import {User} from "../shared/user";
 import {AuthService} from "../login/auth.service";
 import {timeInterval} from "rxjs/operators";
+import {Movie} from "../shared/movie";
+import {AngularFireDatabase} from "@angular/fire/database";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {AngularFireAuth} from "@angular/fire/auth";
 
 @Component({
   selector: 'app-test',
@@ -11,14 +15,24 @@ import {timeInterval} from "rxjs/operators";
 })
 export class TestPage implements OnInit {
 
+  private testMovie: Movie = {
+    title: 'Your Name',
+    movieID: 372058,
+    comment: 'My Favorite movie ever! Truly!',
+    rating: 10,
+    hasSeen: true
+  };
+
   constructor(private firebase: FirebaseService,
-              private authServ: AuthService) { }
+              private authServ: AuthService,
+              private db: AngularFirestore,
+              private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
 
-  async methOne(){
-      this.authServ.refreshUserInfo();
+  methOne(){
+      // this.db.collection('users').doc(this.afAuth.auth.currentUser.uid);
   }
 
 }
