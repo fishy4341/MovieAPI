73b9a0f367e727fec634c0ed51a5d144cc730923
Jake K <jake.kopenhefer@gmail.com>
2019-01-29 08:59:25 -0700

    add favicon, missing image, and wrap buttons for small screens

--- src/app/details/movie-details/movie-details.page.ts
+++ src/app/details/movie-details/movie-details.page.ts
@@ -1,22 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import {MovieAPIService} from '../../API/movie-api.service';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {SelectedMovieService} from '../../API/selected-movie.service';
 import {ModalController} from '@ionic/angular';
 import {RatingComponent} from './rating/rating.component';
 import {AuthService} from '../../login/auth.service';
-import {Movie} from '../../shared/movie';
-=======
-import {MovieAPIService} from "../../API/movie-api.service";
-import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
-import {SelectedMovieService} from "../../API/selected-movie.service";
-import {ModalController} from "@ionic/angular";
-import {RatingComponent} from "./rating/rating.component";
-import {AuthService} from "../../login/auth.service";
-import {Movie, Movie2} from "../../shared/movie";
-import {FirebaseService} from "../../user-list/firebase.service";
->>>>>>> 3ec713d577948c84a4f69139c12b3e0808086fac
+import {Movie, Movie2} from '../../shared/movie';
+import {FirebaseService} from '../../user-list/firebase.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -29,17 +19,13 @@ export class MovieDetailsPage implements OnInit {
     return this.selectedMovie.movieId;
   }
 
-<<<<<<< HEAD
   constructor(
     private movieApi: MovieAPIService,
     public sanitizer: DomSanitizer,
     private selectedMovie: SelectedMovieService,
     public modalController: ModalController,
-    private auth: AuthService
-    ) { }
-=======
-  constructor(private movieApi: MovieAPIService, public sanitizer: DomSanitizer, private selectedMovie: SelectedMovieService, public modalController: ModalController, private auth: AuthService, private firebase: FirebaseService) { }
->>>>>>> 3ec713d577948c84a4f69139c12b3e0808086fac
+    private auth: AuthService,
+    private firebase: FirebaseService) { }
 
   authenticated;
   id = this.movieId;
@@ -76,13 +62,8 @@ export class MovieDetailsPage implements OnInit {
     await modal.present();
     const { data } = await modal.onDidDismiss();
     console.log(data);
-<<<<<<< HEAD
-    const movieData: Movie = {
-      title: data.movie,
-=======
-    let movieData: Movie2 = {
+    const movieData: Movie2 = {
       title: data.title,
->>>>>>> 3ec713d577948c84a4f69139c12b3e0808086fac
       movieID: data.movieId,
       rating: data.rating,
       pic: data.pic,
@@ -92,32 +73,13 @@ export class MovieDetailsPage implements OnInit {
   }
 
   addToSee() {
-<<<<<<< HEAD
-    let movie = {};
-    this.movie$.subscribe(data => {
-      movie = data;
-      console.log(movie);
-      const movieData: Movie = {
-        // @ts-ignore
-        title: movie.title,
-        // @ts-ignore
-        movieID: movie.id,
-        rating: 0,
-        comment: '',
-        hasSeen: false,
-      };
-      this.auth.addMovieToUser(movieData);
-      this.checkWatched();
-    });
-=======
-    let movieData: Movie2 = {
+    const movieData: Movie2 = {
       title: this.movie.title,
       movieID: this.movie.id,
       pic: this.movie.poster_path,
       genres: this.movie.genres,
     };
     this.firebase.pushToSee(movieData);
->>>>>>> 3ec713d577948c84a4f69139c12b3e0808086fac
 
   }
 
