b3272d41070e8149b56533a42c8b2767a088ecd2
Jake K <jake.kopenhefer@gmail.com>
2019-02-06 10:12:24 -0700

    create getter for ID

--- src/app/API/movie-api.service.ts
+++ src/app/API/movie-api.service.ts
@@ -14,11 +14,9 @@ export class MovieAPIService {
  // tslint:disable-next-line:max-line-length
  // readToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjU4Y2U3OTA5YTAxOWMyNzdiZmQzYWU4MTk0ZTJiZiIsInN1YiI6IjVjNDczYTQ2MGUwYTI2NDk1ZGNhMWI4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J6i5lgiN-BAiYVPaREWXS1MMEpD7r4i0XMrjCKZ0rko';
 
-  searchMovies(searchTerms: string) {
-    // tslint:disable-next-line:max-line-length
+  searchMovies(searchTerms: string): Observable<any> {
     return this.http.get(`${this.baseUrl}search/movie?${this.apiKey}&language=en-US&query=${searchTerms}&page=1&include_adult=false`);
   }
-
   getTopRated(page) {
     return this.http.get(`${this.baseUrl}movie/top_rated?${this.apiKey}&language=en-US&page=${page}`);
   }
@@ -34,30 +32,19 @@ export class MovieAPIService {
   getUpcoming(page) {
     return this.http.get(`${this.baseUrl}movie/upcoming?${this.apiKey}&language=en-US&page=${page}`);
   }
-
-
   getMovieDetail(id: number) {
     return this.http.get(`${this.baseUrl}movie/${id}?${this.apiKey}&language=en-US`);
   }
-
   getMovieVideo(id: number) {
     return this.http.get(`${this.baseUrl}movie/${id}/videos?${this.apiKey}&language=en-US`);
   }
-
   getgenreIds() {
     return this.http.get(`${this.baseUrl}genre/movie/list?${this.apiKey}&language=en-US`);
   }
-
   getRelated(id: number) {
     return this.http.get(`${this.baseUrl}movie/${id}/similar?${this.apiKey}&language=en-US`);
   }
-
   getRecommended(id: number) {
     return this.http.get(`${this.baseUrl}movie/${id}/recommendations?${this.apiKey}&language=en-US`);
   }
-
-
 }
-
-
-
